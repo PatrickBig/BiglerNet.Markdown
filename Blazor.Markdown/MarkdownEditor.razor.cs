abe3894a5ce063e69a57298959925d6b06f18d9e
Patrick Bigler <patrick.bigler@ideaintegrations.com>
2021-05-16 10:25:09 -0500

    Add project files.

---
+++ Blazor.Markdown/MarkdownEditor.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Markdown
+{
+    public partial class MarkdownEditor : InputBase<string>
+    {
+        [Parameter]
+        public string Rows { get; set; }
+
+        protected override bool TryParseValueFromString(string value, [MaybeNullWhen(false)] out string result, [NotNullWhen(false)] out string validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+
+        private void Keypress(KeyboardEventArgs args)
+        {
+            Console.WriteLine("Logging");
+        }
+
+        private void SelectionChanged(EventArgs e)
+        {
+            Console.WriteLine("Here");
+        }
+    }
+}
---
+++ Blazor.Markdown/TextSelectionInformation.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazor.Markdown
+{
+    /// <summary>
+    /// Information about the currently selected text in the editor control.
+    /// </summary>
+    public class TextSelectionInformation
+    {
+        /// <summary>
+        /// Gets or sets the currently selected text in editor control.
+        /// </summary>
+        public string SelectedText { get; set; }
+
+        /// <summary>
+        /// Gets or sets the starting location of the selected text.
+        /// </summary>
+        public int SelectionStart { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end location of the selected text.
+        /// </summary>
+        public int SelectionEnd { get; set; }
+    }
+}
