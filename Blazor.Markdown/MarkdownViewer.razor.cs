aa40e7d35a275e0e8a7774a99801719a4271b2d8
Patrick Bigler <patrick.bigler@ideaintegrations.com>
2021-05-16 16:58:03 -0500

    Renamed component to add consistency Added github action for publish

---
+++ Blazor.Markdown/MarkdownViewer.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Markdown
+{
+    public partial class MarkdownViewer : ComponentBase
+    {
+        private string _content;
+
+        [Parameter]
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                _content = value;
+                Html = Markdig.Markdown.ToHtml(Content);
+            }
+        }
+
+        public string Html { get; private set; }
+    }
+}
